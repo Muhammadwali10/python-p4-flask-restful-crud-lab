7ae8a17a4ed9c8cf6b49f1d5c96d5614e6f4077a
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-10-21 18:19:04 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,55 +1,42 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-
+from flask_migrate import Migrate
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-db.init_app(app)
 
 api = Api(app)
+migrate = Migrate(app, db)
 
+db.init_app(app)
 
-class Plants(Resource):
+class PlantsResource(Resource):
+    def get(self, plant_id):
+        plant = db.session.get(Plant, plant_id)
+        if plant:
+            return jsonify(plant.to_dict())
+        return {'message': 'Plant not found'}, 404
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+    def patch(self, plant_id):
+        plant = db.session.get(Plant, plant_id)
+        if plant:
+            data = request.get_json()
+            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+            db.session.commit()
+            return jsonify(plant.to_dict())
+        return {'message': 'Plant not found'}, 404
 
-    def post(self):
-        data = request.get_json()
+    def delete(self, plant_id):
+        plant = db.session.get(Plant, plant_id)
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return '', 204
+        return {'message': 'Plant not found'}, 404
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+api.add_resource(PlantsResource, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
